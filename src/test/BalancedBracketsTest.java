180a0f637ea7fb47215ee1991f4e02e65fc3fcb5
AlexSitja <146761368+AlexSitja@users.noreply.github.com>
2024-03-15 14:12:23 +0100

    Add files via upload

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,74 @@
 package test;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
 
-    //TODO: add tests here
+    // Test per a una cadena amb claudàtors emparellats correctament
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void testBalancedBrackets() {
+        assertTrue(BalancedBrackets.isBalanced("(){}[]"));
     }
 
+    // Test per a claudàtors imbricats
+    @Test
+    public void testNestedBrackets() {
+        assertTrue(BalancedBrackets.isBalanced("{[()]}"));
+    }
 
+    // Test per a claudàtors amb ordre incorrecte
+    @Test
+    public void testIncorrectBracketOrder() {
+        assertFalse(BalancedBrackets.isBalanced("{[)}"));
+    }
+
+    // Test per a altres caràcters que no afecten l'avaluació
+    @Test
+    public void testOtherCharactersNotAffectEvaluation() {
+        assertTrue(BalancedBrackets.isBalanced("abc(def)ghi[jkl]mno"));
+    }
+
+    // Test per a una cadena buida
+    @Test
+    public void testEmptyString() {
+        assertTrue(BalancedBrackets.isBalanced(""));
+    }
+
+    // Test per a una cadena sense claudàtors
+    @Test
+    public void testNoBrackets() {
+        assertTrue(BalancedBrackets.isBalanced("abcdefghi"));
+    }
+
+    // Test per a claudàtors incomplets
+    @Test
+    public void testIncompleteBrackets() {
+        assertFalse(BalancedBrackets.isBalanced("{[(])}"));
+    }
+
+    // Test per a múltiples claudàtors
+    @Test
+    public void testMultipleBrackets() {
+        assertTrue(BalancedBrackets.isBalanced("{[()]}"));
+    }
+
+    // Test per a claudàtors desequilibrats al principi
+    @Test
+    public void testUnbalancedBracketsAtStart() {
+        assertFalse(BalancedBrackets.isBalanced("}{[]()}"));
+    }
+
+    // Test per a claudàtors desequilibrats al final
+    @Test
+    public void testUnbalancedBracketsAtEnd() {
+        assertFalse(BalancedBrackets.isBalanced("{[()]}"));
+    }
+
+    // Test per a claudàtors desequilibrats al mig
+    @Test
+    public void testUnbalancedBracketsInMiddle() {
+        assertFalse(BalancedBrackets.isBalanced("{[()]}{}"));
+    }
 }
+
