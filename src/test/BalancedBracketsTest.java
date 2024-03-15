c7ba67ffa666107203aa42af7b51525861f47b9e
AlexSitja <146761368+AlexSitja@users.noreply.github.com>
2024-03-15 14:28:22 +0100

    Add files via upload

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import org.junit.Test;
+import main.BalancedBrackets;
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
@@ -8,67 +9,67 @@ public class BalancedBracketsTest {
     // Test per a una cadena amb claudàtors emparellats correctament
     @Test
     public void testBalancedBrackets() {
-        assertTrue(BalancedBrackets.isBalanced("(){}[]"));
+        assertTrue(BalancedBrackets.hasBalancedBrackets("(){}[]"));
     }
 
     // Test per a claudàtors imbricats
     @Test
     public void testNestedBrackets() {
-        assertTrue(BalancedBrackets.isBalanced("{[()]}"));
+        assertTrue(BalancedBrackets.hasBalancedBrackets("{[()]}"));
     }
 
     // Test per a claudàtors amb ordre incorrecte
     @Test
     public void testIncorrectBracketOrder() {
-        assertFalse(BalancedBrackets.isBalanced("{[)}"));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("{[)}"));
     }
 
     // Test per a altres caràcters que no afecten l'avaluació
     @Test
     public void testOtherCharactersNotAffectEvaluation() {
-        assertTrue(BalancedBrackets.isBalanced("abc(def)ghi[jkl]mno"));
+        assertTrue(BalancedBrackets.hasBalancedBrackets("abc(def)ghi[jkl]mno"));
     }
 
     // Test per a una cadena buida
     @Test
     public void testEmptyString() {
-        assertTrue(BalancedBrackets.isBalanced(""));
+        assertTrue(BalancedBrackets.hasBalancedBrackets(("")));
     }
 
     // Test per a una cadena sense claudàtors
     @Test
     public void testNoBrackets() {
-        assertTrue(BalancedBrackets.isBalanced("abcdefghi"));
+        assertTrue(BalancedBrackets.hasBalancedBrackets(("abcdefghi")));
     }
 
     // Test per a claudàtors incomplets
     @Test
     public void testIncompleteBrackets() {
-        assertFalse(BalancedBrackets.isBalanced("{[(])}"));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("{[(])}"));
     }
 
     // Test per a múltiples claudàtors
     @Test
     public void testMultipleBrackets() {
-        assertTrue(BalancedBrackets.isBalanced("{[()]}"));
+        assertTrue(BalancedBrackets.hasBalancedBrackets("{[()]}"));
     }
 
     // Test per a claudàtors desequilibrats al principi
     @Test
     public void testUnbalancedBracketsAtStart() {
-        assertFalse(BalancedBrackets.isBalanced("}{[]()}"));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("}{[]()}"));
     }
 
     // Test per a claudàtors desequilibrats al final
     @Test
     public void testUnbalancedBracketsAtEnd() {
-        assertFalse(BalancedBrackets.isBalanced("{[()]}"));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("{[()]}"));
     }
 
     // Test per a claudàtors desequilibrats al mig
     @Test
     public void testUnbalancedBracketsInMiddle() {
-        assertFalse(BalancedBrackets.isBalanced("{[()]}{}"));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("{[()]}{}"));
     }
 }
 
